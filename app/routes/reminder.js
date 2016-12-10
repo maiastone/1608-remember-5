cf6121c6e652173de8fd2627a9b3f92bde8740ac
Maia Stone <maiadov@gmail.com>
2016-12-10 15:05:56 -0700

    Saves edited reminder

--- app/routes/reminder.js
+++ app/routes/reminder.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').find('reminder', params.reminder_id);
+  },
+  model() {
+    this.store.set({
+      title: model.title,
+      notes: model.notes,
+      date: model.date
+    });
   }
 });
