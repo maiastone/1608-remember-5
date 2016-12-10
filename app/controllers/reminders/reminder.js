6e5147ad1e58a5c5538e3f8e5ae95363f3d26e4b
Maia Stone <maiadov@gmail.com>
2016-12-10 12:54:57 -0700

    Adds action for saveReminder on submit
    Adds saveReminder action on the save button which is displayed if toggleProperty is set to isEditing.  The type of event is  on 'submit'

--- app/controllers/reminders/reminder.js
+++ app/controllers/reminders/reminder.js
@@ -4,6 +4,19 @@ export default Ember.Controller.extend({
   actions: {
     editReminder: function() {
       this.toggleProperty('isEditing');
-    }
+    },
+    saveReminder: function(model) {
+      let reminder = model.getProperties('title', 'notes', 'date');
+      this.get('store').findRecord('reminder', model.id)
+      .then(function(activeReminder) {
+         activeReminder.setProperties({
+           title: reminder.title,
+           notes: reminder.notes,
+           date: reminder.date,
+         });
+         activeReminder.save();
+       });
+       this.toggleProperty('isEditing');
+      },
   }
 });
