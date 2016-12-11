83b3e51431480e90abc1e43d6db95e87bbd5c926
Maia Stone <maiadov@gmail.com>
2016-12-11 09:24:39 -0700

    Removes unused code

--- app/routes/reminder.js
+++ app/routes/reminder.js
@@ -4,11 +4,4 @@ export default Ember.Route.extend({
   model(params) {
     return this.get('store').find('reminder', params.reminder_id);
   },
-  model() {
-    this.store.set({
-      title: model.title,
-      notes: model.notes,
-      date: model.date
-    });
-  }
 });
