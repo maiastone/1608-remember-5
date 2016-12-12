b683699f145f23925c31358274c05ec5fd299907
Maia Stone <maiadov@gmail.com>
2016-12-11 18:24:19 -0700

    Tests passing for default home page and reminders route

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -7,31 +7,37 @@ import Ember from 'ember';
 
 moduleForAcceptance('Acceptance | reminders list');
 
-test('viewing the homepage', function(assert) {
+test('viewing the reminders page', function(assert) {
   server.createList('reminder', 5);
-  visit('/');
+  visit('/reminders');
   andThen(function() {
     assert.equal(currentURL(), '/reminders');
     assert.equal(Ember.$('.spec-reminder-item').length, 5);
   });
 });
 
-test('viewing the homepage', function(assert) {
+test('viewing the reminders', function(assert) {
   server.createList('reminder', 10);
-  visit('/');
+  visit('/reminders');
   andThen(function() {
     assert.equal(Ember.$('.spec-reminder-item').length, 10);
   });
 });
 
+test('viewing a default welcome page', function(assert) {
+  server.createList('reminder', 10);
+  visit('/');
+  andThen(function() {
+    assert.equal(find('.spec-title').text(), 'remEMBER');
+  });
+});
+
 test('clicking on an individual item', function(assert) {
   server.createList('reminder', 5);
-  visit('/');
+  visit('/reminders/');
   click('.spec-reminder-item:first');
-
   andThen(function() {
     assert.equal(currentURL(), '/reminders/1');
     assert.equal(Ember.$('.spec-reminder-item:first').text().trim(), Ember.$('.spec-reminder-title').text().trim());
   });
-
 });
