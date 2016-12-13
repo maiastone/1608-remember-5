cb792a44b77abce3f014f052c6ae5573811f643b
Maia Stone <maiadov@gmail.com>
2016-12-13 12:35:39 -0700

    Refactors edit and new into reminder form

---
+++ app/routes/reminders/new.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return this.get('store').createRecord('reminder');
+  },
+  actions: {
+    saveReminder() {
+      this.transitionTo('reminders');
+    }
+  }
+});
