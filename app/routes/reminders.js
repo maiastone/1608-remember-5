81795647b895031c2955b7b92c2290797799ea89
Maia Stone <maiadov@gmail.com>
2016-12-14 07:37:39 -0700

    Adds delete functionality

--- app/routes/reminders.js
+++ app/routes/reminders.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.get('store').findAll('reminder');
+  },
+  actions: {
+    delete(model) {
+      model.destroyRecord('reminder', model.id);
+      this.transitionTo('reminders');
+    }
   }
 });
